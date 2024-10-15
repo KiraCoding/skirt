eb80d77acf160a788605bc2c3237d8fdfc9c80f6
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-10-15 15:38:07 +0200

    wip

---
+++ src/mutex.rs
@@ -0,0 +1,114 @@
+use core::cell::UnsafeCell;
+use core::fmt::{Debug, Display, Formatter};
+use core::hint::spin_loop;
+use core::ops::{Deref, DerefMut};
+use core::sync::atomic::{AtomicBool, Ordering};
+
+pub struct Mutex<T: ?Sized> {
+    lock: AtomicBool,
+    data: UnsafeCell<T>,
+}
+
+unsafe impl<T: ?Sized + Send> Send for Mutex<T> {}
+unsafe impl<T: ?Sized + Send> Sync for Mutex<T> {}
+
+impl<T> Mutex<T> {
+    pub fn new(data: T) -> Self {
+        Self {
+            lock: AtomicBool::new(false),
+            data: UnsafeCell::new(data),
+        }
+    }
+}
+
+impl<T: ?Sized> Mutex<T> {
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        while self
+            .lock
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {
+            spin_loop();
+        }
+
+        MutexGuard::new(self)
+    }
+
+    pub fn try_lock(&self) -> Option<MutexGuard<'_, T>> {
+        match self
+            .lock
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+        {
+            Ok(_) => Some(MutexGuard::new(self)),
+            Err(_) => None,
+        }
+    }
+}
+
+impl<T> From<T> for Mutex<T> {
+    fn from(data: T) -> Self {
+        Self::new(data)
+    }
+}
+
+impl<T: ?Sized + Default> Default for Mutex<T> {
+    fn default() -> Self {
+        Self::new(T::default())
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for Mutex<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        let mut d = f.debug_struct("Mutex");
+
+        match self.try_lock() {
+            Some(guard) => d.field("data", &&*guard),
+            None => d.field("data", &format_args!("<locked>")),
+        };
+
+        d.finish_non_exhaustive()
+    }
+}
+
+pub struct MutexGuard<'m, T: ?Sized + 'm> {
+    mutex: &'m Mutex<T>,
+}
+
+impl<'m, T: ?Sized> MutexGuard<'m, T> {
+    fn new(mutex: &'m Mutex<T>) -> Self {
+        Self { mutex }
+    }
+}
+
+impl<T: ?Sized> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.mutex.data.get() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.mutex.data.get() }
+    }
+}
+
+impl<T: ?Sized> Drop for MutexGuard<'_, T> {
+    #[inline]
+    fn drop(&mut self) {
+        self.mutex.lock.store(false, Ordering::Release);
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for MutexGuard<'_, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        Debug::fmt(&**self, f)
+    }
+}
+
+impl<T: ?Sized + Display> Display for MutexGuard<'_, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        Display::fmt(&**self, f)
+    }
+}
